91afd76a7293b4a57f5880b71ff250e8770cda6d
moonorange <monoma632@gmail.com>
2020-06-07 22:18:16 +0900

    fix: typo

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,32 +1,31 @@
-import Layout from '../../componetns/layout'
-import { getAllPostsIds, getPostData } from '../../lib/posts'
-
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post({ postData }) {
-    return (
-      <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-      </Layout>
-    )
-  }
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
+}
 
 export async function getStaticPaths() {
-    const paths = getAllPostsIds()
-    return {
-        paths,
-        fallback: false
-    }
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
 }
 
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id)
-    return {
-        props: {
-            postData
-        }
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
     }
+  }
 }
