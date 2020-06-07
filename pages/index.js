a40c73269622b3327cb9f44e6b85bfdcc2c4d757
moonorange <monoma632@gmail.com>
2020-06-07 11:50:49 +0900

    add: fetched external data and pre-rendered the index page with this data

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -12,9 +22,23 @@ export default function Home() {
         <p>Hi, My name is Keigo Kida. I was born and raised in Osaka, Japan. I currently live in Tokyo and work as a software engineer in a start-up.</p>
         <p>
           <a href="https://github.com/moonorange" target="_blank">Github</a>
-          <tr></tr>
+          <br />
           <a href="https://www.linkedin.com/in/keigo-k-70557314b/" target="_blank">Linkedin</a>
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
